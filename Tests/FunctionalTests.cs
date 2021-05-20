8549e0ce7747f66b851c35e6072d1b582fe678e9
Julie Song <t-juliesong@microsoft.com>
2021-05-20 18:22:40 -0400

    initial commit

---
+++ Tests/FunctionalTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace NETTests.Tests
+{
+    class FunctionalTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            driver = new FirefoxDriver(@"C:\Users\t-juliesong\.nuget\packages\selenium.firefox.webdriver\0.27.0\driver");;
+        }
+        
+        [Test]
+        public void TitleTest()
+        {
+            driver.Url = "https://t-juliesongcloud-staging.azurewebsites.net/";
+            IWebElement element = driver.FindElement(By.Id("title"));
+            String val = element.GetAttribute("innerText");
+            Assert.IsTrue(val.Contains("asdf"));
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            Console.WriteLine("Opening site at URL...");
+            driver.Url = "https://t-juliesongcloud-staging.azurewebsites.net/";
+        }
+        
+        [Test]
+        public void HomeExistCheck()
+        {
+            Console.WriteLine("Finding Home Button...");
+            driver.Url = "https://t-juliesongcloud-staging.azurewebsites.net/";
+            driver.FindElement(By.Id("home-button"));
+        }
+
+        [Test]
+        public void AboutExistCheck()
+        {
+            Console.WriteLine("Finding About Button...");
+            driver.Url = "https://t-juliesongcloud-staging.azurewebsites.net/";
+            driver.FindElement(By.Id("about-button"));
+        }
+
+        [Test]
+        public void ContactExistCheck()
+        {
+            Console.WriteLine("Finding Contact Button...");
+            driver.Url = "https://t-juliesongcloud-staging.azurewebsites.net/";
+            driver.FindElement(By.Id("contact-button"));
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            Console.WriteLine("Closing driver...");
+            driver.Close();
+        }
+    }
+}
